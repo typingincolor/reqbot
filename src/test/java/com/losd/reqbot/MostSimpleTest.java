5d938e9e457c4d919e1ac48f9c527e14ab2aa90e
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2015-05-01 16:48:37 +0100

    activate travis

---
+++ src/test/java/com/losd/reqbot/MostSimpleTest.java
@@ -0,0 +1,15 @@
+package com.losd.reqbot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by abraithwaite on 01/05/15.
+ */
+public class MostSimpleTest {
+    @Test
+    public void simpleTest() {
+        assertEquals(2, 1+1);
+    }
+}
