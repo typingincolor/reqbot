3e0f07a0f12647ef875589250b428cd6f198bd24
Andrew Braithwaite <abraithw@gmail.com>
2015-05-05 21:31:54 +0100

    added first redis test

---
+++ src/test/java/com/losd/reqbot/repository/BucketRedisRepoTest.java
@@ -0,0 +1,71 @@
+package com.losd.reqbot.repository;
+
+import com.losd.reqbot.model.Request;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import redis.clients.jedis.Jedis;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2015 Andrew Braithwaite
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+public class BucketRedisRepoTest {
+    @Mock
+    Jedis jedis;
+
+    @InjectMocks
+    BucketRedisRepo repo;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getBucketForUsers() {
+        Set<String> mockResult = new HashSet<>(Arrays.asList("a", "b"));
+        when(jedis.smembers("homer")).thenReturn(mockResult);
+
+        Set<String> buckets = repo.getBucketsForUser("homer");
+
+        verify(jedis, times(1)).smembers("homer");
+
+        assertThat(buckets, is(equalTo(mockResult)));
+    }
+}
