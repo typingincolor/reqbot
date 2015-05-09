92d001f01d349e63f3fde46435b9792cb3de54fc
Andrew Braithwaite <abraithw@gmail.com>
2015-05-09 15:48:58 +0100

    get styling...

--- src/test/ruby/smoke_test.rb
+++ src/test/ruby/smoke_test.rb
@@ -1,6 +1,8 @@
 require 'rest-client'
 require 'json'
 
+bucket = ARGV[0] || "andrew"
+
 random_body = (0...10).map { ('a'..'z').to_a[rand(26)] }.join
 programmed_response = {'headers' => {'header1' => 'value1'}, 'body' => random_body}
 saved_response_result = RestClient.post 'http://localhost:8080/response',
@@ -11,10 +13,10 @@ r = JSON.parse(saved_response_result)
 
 response_received = []
 
-response_received.push RestClient.get 'http://localhost:8080/bucket/andrew/response/' + r['uuid']
-response_received.push RestClient.get 'http://localhost:8080/bucket/andrew/path/to/something', 'X-REQBOT-RESPONSE' => r['uuid']
-response_received.push RestClient.post 'http://localhost:8080/bucket/andrew/response/' + r['uuid'], "stuff", :content_type => 'text/plain'
-response_received.push RestClient.post 'http://localhost:8080/bucket/andrew/path/to/somethingelse', "stuff",:content_type => 'text/plain', 'X-REQBOT-RESPONSE' => r['uuid']
+response_received.push RestClient.get "http://localhost:8080/bucket/#{bucket}/response/#{r['uuid']}"
+response_received.push RestClient.get "http://localhost:8080/bucket/#{bucket}/path/to/something", 'X-REQBOT-RESPONSE' => r['uuid']
+response_received.push RestClient.post "http://localhost:8080/bucket/#{bucket}/response/#{r['uuid']}", "stuff", :content_type => 'text/plain'
+response_received.push RestClient.post "http://localhost:8080/bucket/#{bucket}/path/to/somethingelse", "stuff",:content_type => 'text/plain', 'X-REQBOT-RESPONSE' => r['uuid']
 
 response_received.each_with_index do |result, i|
   puts "#{i} worked: #{random_body.eql? result}     #{random_body} #{result}"
