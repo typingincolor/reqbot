e726e3237d8138548664211125907df41608ad93
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2015-05-13 11:56:45 +0100

    sort tags alphabetically

--- src/main/java/com/losd/reqbot/repository/ResponseRedisRepo.java
+++ src/main/java/com/losd/reqbot/repository/ResponseRedisRepo.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Transaction;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -99,6 +100,7 @@ public class ResponseRedisRepo implements ResponseRepo {
         List<String> result = new LinkedList<>();
 
         tags.forEach((tag) -> result.add(tag.substring(TAG_PREFIX.length())));
+        Collections.sort(result);
         return result;
     }
 }
